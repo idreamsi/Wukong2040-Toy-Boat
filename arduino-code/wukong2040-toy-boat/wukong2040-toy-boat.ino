3ff187ea8948e984052d6a4b721d7a822650fc2d
Infinite Dreams <8011806+idreamsi@users.noreply.github.com>
2023-11-15 13:04:48 +0330

    Update wukong2040-toy-boat.ino

--- arduino-code/wukong2040-toy-boat/wukong2040-toy-boat.ino
+++ arduino-code/wukong2040-toy-boat/wukong2040-toy-boat.ino
@@ -1,4 +1,5 @@
 /*******************************************************************************
+ *
  * Wukong2040 Toy Boat 
  * Build a wifi controlled toy boat with Wukong2040 and Raspberry Pi Pico W
  * 
